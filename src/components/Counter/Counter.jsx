b8f7b1a79de7ec4f9e3e210c9db517032ad8b2c3
Pete Seeling <peteseeling@gmail.com>
2022-05-11 18:11:26 -0600

    test passing. added find color.

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,24 +1,28 @@
 import { useEffect, useState, useReducer } from 'react';
 
-const pinkRGB = `rgb(236, 72, 153)`
 const color = {
   green: `rgb(52, 211, 153)`,
    red: `rgb(239, 68, 68)`,
    pink: `rgb(236, 72, 153)`
   }
 
-const initailState = { count: 0, currentColor: color.green}
+const initailState = { count: 0, currentColor: color.pink}
 
-
+function findColor(count) {
+ if (count === 0) return color.pink;
+ if(count < 0) return color.red;
+ if(count > 0) return color.green;
+ return color
+}
 
 function reducer(state, action){
 switch (action.type) {
   case 'increment':
-    return { count: state.count + 1 };
+    return { count: state.count + 1, currentColor: findColor(state.count + 1) };
     case 'decrement':
-      return { count: state.count - 1 };
+      return { count: state.count - 1,  currentColor: findColor(state.count - 1) };
       case 'reset':
-        return { count: 0 };
+        return { count: 0, currentColor: findColor(0) };
           default: 
         return { count: state.count }
 }
@@ -27,21 +31,21 @@ switch (action.type) {
 export default function Counter() {
   // const [count, setCount] = useState(0)
   const [state, dispatch] = useReducer(reducer, initailState)
-  const [currentColor, setCurrentColor] = useState(pinkRGB)
+  const [currentColor, setCurrentColor] = useState(color.pink)
 
-  useEffect(() => {
-    if (state === 0) {
-      setCurrentColor(pinkRGB)
-    }
+  // useEffect(() => {
+  //   if (state === 0) {
+  //     setCurrentColor(pinkRGB)
+  //   }
 
-    if (state > 0) {
-      setCurrentColor(color.green)
-    }
+  //   if (state > 0) {
+  //     setCurrentColor(color.green)
+  //   }
 
-    if (state < 0) {
-      setCurrentColor(color.red)
-    }
-  }, [state])
+  //   if (state < 0) {
+  //     setCurrentColor(color.red)
+  //   }
+  // }, [state])
 
 return (
   <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
